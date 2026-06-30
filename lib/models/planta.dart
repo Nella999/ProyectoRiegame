464ea449350cf77237448a496d0de86682311edf
Nella999 <balora@ulasalle.edu.pe>
2026-06-29 23:34:48 -0500

    Agregando map de Firestore

--- lib/models/planta.dart
+++ lib/models/planta.dart
@@ -1,15 +1,37 @@
 class Planta {
+  //Detallando la info registrada
   final String id;
   final String nombre;
   final String apodo;
   final String observaciones;
-  final String frecuenciaRiego;
+  final int frecuenciaDias;
 
   Planta({
     required this.id,
     required this.nombre,
     required this.apodo,
     required this.observaciones,
-    required this.frecuenciaRiego,
+  required this.frecuenciaDias,
   });
+  //Agregando map para que Firestore pueda leerlo
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'nombre': nombre,
+      'apodo': apodo,
+      'observaciones': observaciones,
+      'frecuenciaDias': frecuenciaDias,
+    };
+  }
+
+  // Convierte un doc de Firestore en un objeto Planta
+  factory Planta.fromMap(Map<String, dynamic> map) {
+    return Planta(
+      id: map['id'] ?? '',
+      nombre: map['nombre'] ?? '',
+      apodo: map['apodo'] ?? '',
+      observaciones: map['observaciones'] ?? '',
+        frecuenciaDias: map['frecuenciaDias'] ?? 0,
+    );
+  }
 }
